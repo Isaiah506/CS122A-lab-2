cd5b1a5d5eaf91f8eeb177009cc29819a0337697
Sage_Grouse <isaiahtrd@gmail.com>
2026-04-06 19:47:42 -0700

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -1,25 +1,61 @@
 module top (
-    /** Input Ports */
-    /** Output Ports */
-
-    input wire[3:0] bcd,
+    input logic clk,
+    input reg [3:0] bcd,
     output logic [6:0] seg7
 );
 
+
 /** Logic */
-    always @(*) begin
-        seg7[0] <= (!bcd[1] & !bcd[3]) | (bcd[1] & bcd[3]) | (bcd[0] | !bcd[2]) | (!bcd[0] & !bcd[1] & bcd[2]) | (bcd[0] & !bcd[3]);
-        
-        seg7[1] <= (!bcd[1] & !bcd[2]) | (bcd[1] & !bcd[3]) | (!bcd[0] & bcd[2] & bcd[3]);
-        
-        seg7[2] <= (bcd[3]) | (!bcd[0] & ! bcd[2]) | (!bcd[1] & !bcd[2]) & (!bcd[0] & bcd[1]);
-        
-        seg7[3] <= (!bcd[0] & !bcd[1] & !bcd[3]) | (bcd[0] & !bcd[2] & !bcd[3]) | (!bcd[1] & bcd[2] & bcd[3]) | (bcd[1] & !bcd[2] & bcd[3]) | (bcd[1] & bcd[2] & !bcd[3]);
-        
-        seg7[4] <= (!bcd[1] & bcd[2]) | (bcd[0] & bcd[1]) | (!bcd[0] & !bcd[1] & bcd[3]) | (bcd[1] & !bcd[2] & bcd[3]) | (bcd[2] & !bcd[3]);
-        
-        seg7[5] <= (!bcd[0] & !bcd[0]) | (bcd[1] & bcd[3]) | (bcd[0] & bcd[2]);
-        
-        seg7[6] <= (!bcd[0] & bcd[3]) | (bcd[0] & !bcd[3]) | (bcd[2] & bcd[3]) | (bcd[1] & bcd[3]) | (bcd[1] & bcd[2]);
+always @(*) begin
+    if(bcd == 4'b0000) begin
+        seg7 = 7'b1111110;
     end
+    else if(bcd == 4'b0001) begin
+        seg7 = 7'b0110000;
+    end
+    else if(bcd == 4'b0010) begin
+        seg7 = 7'b1101101;
+    end
+    else if(bcd == 4'b0011) begin
+        seg7 = 7'b1111001;
+    end
+    else if(bcd == 4'b0100) begin
+        seg7 = 7'b0110001;
+    end
+    else if(bcd == 4'b0101) begin
+        seg7 = 7'b1110011;
+    end
+    else if(bcd == 4'b0110)begin
+        seg7 = 7'b1011111;
+    end
+    else if(bcd == 4'b0111) begin
+        seg7 = 7'b1110000;
+    end
+    else if(bcd == 4'b1000) begin
+        seg7 = 7'b1111111;
+    end
+    else if(bcd == 4'b1001) begin
+        seg7 = 7'b1111011;
+    end
+    else if(bcd == 4'b1010) begin
+        seg7 = 7'b1110111;
+    end
+    else if(bcd == 4'b1011) begin
+        seg7 = 7'b0001111;
+    end
+    else if(bcd == 4'b1100) begin
+        seg7 = 7'b1001110;
+    end 
+    else if(bcd == 4'b1101) begin
+        seg7 = 7'b0111100;
+    end
+    else if(bcd == 4'b1110) begin
+        seg7 = 7'b1001111;
+    end
+    else if(bcd == 4'b1111) begin
+        seg7 = 7'b1000111;
+    end
+end
+
 endmodule
+
