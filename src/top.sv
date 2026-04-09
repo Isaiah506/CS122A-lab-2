88f70de813f7e5bc6d04d22b938b6d8bef1cf33a
Sage_Grouse <isaiahtrd@gmail.com>
2026-04-08 23:55:41 -0700

    Refactor top module to integrate adder and decoder
    Refactored top module to use adder and decoder instances, removing the BCD logic.

--- src/top.sv
+++ src/top.sv
@@ -1,60 +1,27 @@
+`include "src/adder.sv"
+`include "src/decoder.sv"
+
 module top (
     input logic clk,
-    input reg [3:0] bcd,
-    output logic [6:0] seg7
+    input wire[3:0] val1,
+    input wire[3:0] val2,
+    output logic [7:0] seg7 
+);
+/** Logic */
+reg [4:0] tmp_res;
+
+assign seg7[0] = tmp_res[4];
+
+adder top_adder(
+    .val1(val1),
+    .val2(val2),
+    .res(tmp_res)
 );
 
+decoder top_decoder(
+    .bcd(tmp_res[3:0]),
+    .seg7(seg7[7:1])
+);
 
-/** Logic */ //4,5,b,d
-always @(*) begin
-    if(bcd == 4'b0000) begin
-        seg7 = 7'b1111110;
-    end
-    else if(bcd == 4'b0001) begin
-        seg7 = 7'b0110000;
-    end
-    else if(bcd == 4'b0010) begin
-        seg7 = 7'b1101101;
-    end
-    else if(bcd == 4'b0011) begin
-        seg7 = 7'b1111001;
-    end
-    else if(bcd == 4'b0100) begin
-        seg7 = 7'b0110011;
-    end
-    else if(bcd == 4'b0101) begin
-        seg7 = 7'b1011011;
-    end
-    else if(bcd == 4'b0110)begin
-        seg7 = 7'b1011111;
-    end
-    else if(bcd == 4'b0111) begin
-        seg7 = 7'b1110000;
-    end
-    else if(bcd == 4'b1000) begin
-        seg7 = 7'b1111111;
-    end
-    else if(bcd == 4'b1001) begin
-        seg7 = 7'b1111011;
-    end
-    else if(bcd == 4'b1010) begin
-        seg7 = 7'b1110111;
-    end
-    else if(bcd == 4'b1011) begin
-        seg7 = 7'b0011111;
-    end
-    else if(bcd == 4'b1100) begin
-        seg7 = 7'b1001110;
-    end 
-    else if(bcd == 4'b1101) begin
-        seg7 = 7'b0111101;
-    end
-    else if(bcd == 4'b1110) begin
-        seg7 = 7'b1001111;
-    end
-    else if(bcd == 4'b1111) begin
-        seg7 = 7'b1000111;
-    end
-end
-
+    
 endmodule
